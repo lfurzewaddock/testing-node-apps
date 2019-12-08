eb9d5101e8a49157e1bafe8798e271d6db24a0f8
Leo Furze-Waddock <leo@furze-waddock.com>
2019-12-08 16:32:39 +0000

    testing pure functions solved (3) jest-in-case

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,55 +1,43 @@
 // Testing Pure Functions
 
+import cases from 'jest-in-case'
+
 import {isPasswordAllowed} from '../auth'
 
-const keys = []
-const obj = {
-  '!aBc123': {
-    expected: true,
-    msg: 'given a valid pw of "!aBc123" should return true',
+cases(
+  'isPasswordAllowed',
+  opts => {
+    const result = isPasswordAllowed(opts.testVal)
+    expect(result).toBe(opts.result)
   },
-  'a2c!': {
-    expected: false,
-    msg: 'given an invalid (too short) pw of "a2c!" should return false',
+  {
+    'given a valid pw of "!aBc123" should return true': {
+      testVal: '!aBc123',
+      result: true,
+    },
+    'given an invalid (too short) pw of "a2c!" should return false': {
+      testVal: 'a2c!',
+      result: false,
+    },
+    'given an invalid (no alphabet characters) pw of "123456!" should return false': {
+      testVal: '123456!',
+      result: false,
+    },
+    'given an invalid (no numbers) pw of "ABCdef!" should return false': {
+      testVal: 'ABCdef!',
+      result: false,
+    },
+    'given an invalid (no uppercase letters) pw of "abc123!" should return false': {
+      testVal: 'abc123!',
+      result: false,
+    },
+    'given an invalid (no lowercase letters) pw of "ABC123!" should return false': {
+      testVal: 'ABC123!',
+      result: false,
+    },
+    'given an invalid (no non-alphanumeric characters) pw of "ABCdef123" should return false': {
+      testVal: 'ABCdef123',
+      result: false,
+    },
   },
-  '123456!': {
-    expected: false,
-    msg:
-      'given an invalid (no alphabet characters) pw of "123456!" should return false',
-  },
-  'ABCdef!': {
-    expected: false,
-    msg: 'given an invalid (no numbers) pw of "ABCdef!" should return false',
-  },
-  'abc123!': {
-    expected: false,
-    msg:
-      'given an invalid (no uppercase letters) pw of "abc123!" should return false',
-  },
-  'ABC123!': {
-    expected: false,
-    msg:
-      'given an invalid (no lowercase letters) pw of "ABC123!" should return false',
-  },
-  ABCdef123: {
-    expected: false,
-    msg:
-      'given an invalid (no non-alphanumeric characters) pw of "ABCdef123" should return false',
-  },
-}
-for (const key in obj) {
-  // eslint-disable-next-line jest/no-if
-  if (Object.prototype.hasOwnProperty.call(obj, key)) {
-    keys.push(key)
-  }
-}
-
-keys.forEach(testVal => {
-  const testDetails = obj[testVal]
-
-  // eslint-disable-next-line jest/valid-title
-  test(testDetails.msg, () => {
-    const result = isPasswordAllowed(testVal)
-    expect(result).toBe(testDetails.expected)
-  })
-})
+)
