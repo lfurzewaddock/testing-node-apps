d36cc01367ab854e1e99ac5e43e6f357d58923c1
Leo Furze-Waddock <leo@furze-waddock.com>
2019-12-08 17:21:59 +0000

    testing pure functions solved (4) cases titles

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -4,40 +4,41 @@ import cases from 'jest-in-case'
 
 import {isPasswordAllowed} from '../auth'
 
+function casify(obj) {
+  return Object.entries(obj).map(([name, testVal]) => {
+    return {
+      name: `${testVal} - ${name}`,
+      testVal,
+    }
+  })
+}
+
 cases(
-  'isPasswordAllowed',
+  'isPasswordAllowed: valid passwords',
   opts => {
     const result = isPasswordAllowed(opts.testVal)
-    expect(result).toBe(opts.result)
+    expect(result).toBe(true)
   },
-  {
-    'given a valid pw of "!aBc123" should return true': {
-      testVal: '!aBc123',
-      result: true,
-    },
-    'given an invalid (too short) pw of "a2c!" should return false': {
-      testVal: 'a2c!',
-      result: false,
-    },
-    'given an invalid (no alphabet characters) pw of "123456!" should return false': {
-      testVal: '123456!',
-      result: false,
-    },
-    'given an invalid (no numbers) pw of "ABCdef!" should return false': {
-      testVal: 'ABCdef!',
-      result: false,
-    },
-    'given an invalid (no uppercase letters) pw of "abc123!" should return false': {
-      testVal: 'abc123!',
-      result: false,
-    },
-    'given an invalid (no lowercase letters) pw of "ABC123!" should return false': {
-      testVal: 'ABC123!',
-      result: false,
-    },
-    'given an invalid (no non-alphanumeric characters) pw of "ABCdef123" should return false': {
-      testVal: 'ABCdef123',
-      result: false,
-    },
+  casify({'given a valid pw of "!aBc123" should return true': '!aBc123'}),
+)
+
+cases(
+  'isPasswordAllowed: invalid passwords',
+  opts => {
+    const result = isPasswordAllowed(opts.testVal)
+    expect(result).toBe(false)
   },
+  casify({
+    'given an invalid (too short) pw of "a2c!" should return false': 'a2c!',
+    'given an invalid (no alphabet characters) pw of "123456!" should return false':
+      '123456!',
+    'given an invalid (no numbers) pw of "ABCdef!" should return false':
+      'ABCdef!',
+    'given an invalid (no uppercase letters) pw of "abc123!" should return false':
+      'abc123!',
+    'given an invalid (no lowercase letters) pw of "ABC123!" should return false':
+      'ABC123!',
+    'given an invalid (no non-alphanumeric characters) pw of "ABCdef123" should return false':
+      'ABCdef123',
+  }),
 )
